cb18a04d35889659037daaa8d6bb1302006a1763
HTML50 <iambenf@gmail.com>
2017-08-19 00:30:52 +0800

    force push

---
+++ ping.js
@@ -0,0 +1,61 @@
+'use strit'
+function Ping(param){
+      this.url = param.url || '' ;
+      this.frequency = param.frequency || 5000;
+      this.mode = param.mode || 'graph';
+      this.node = param.node;
+ 
+    this.stop = function(){
+      clearTimeout(ID);
+      return 'ping stopped'
+    }
+    
+    this.start = function(){
+      ping();
+      return 'ping started'
+    }
+    
+    
+      let img = new Image(),
+      _this = this,
+      beginTime,time,ID;
+    
+    
+      img.onload = reply;
+      img.onerror = reply;
+    
+      
+    function ping(){
+       beginTime = new Date().getTime();
+       img.src = _this.url +'?' + beginTime ;
+    }
+    
+    function reply(e){
+      time = new Date().getTime() - beginTime;
+      ID = setTimeout(ping,_this.frequency);
+      var output;
+      
+      if(_this.mode === 'text'){
+        output = time +' ms';
+      }else{
+        var img;
+        if(time > 2000){
+          img = 'error'
+        }else if(time>1000){
+          img = 'bad'
+        }else if(time>500){
+          img = 'slow'
+        }else if(time>300){
+          img = 'fine'
+        }else if(time>50){
+          img = 'good'
+        }else{
+          img = 'excellent'
+        }
+        output = '<img src="img/'+img+'.png" title="'+time+' ms">'
+      }
+      _this.node.innerHTML = output;
+    }
+     
+    return this.start();
+}
